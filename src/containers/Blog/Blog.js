c7bab80b37e39d70ee65a58e74076ab99ce1e768
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-13 13:43:06 -0800

    added author to each user data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,11 +12,18 @@ class Blog extends Component {
   };
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Andrew",
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
   render() {
-    const posts = this.state.posts.map((post) => <Post title={post.title} key={post.id} />);
+    const posts = this.state.posts.map((post) => <Post title={post.title} author={post.author} key={post.id} />);
     return (
       <div>
         <section className="Posts">{posts}</section>
