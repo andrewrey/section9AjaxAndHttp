636ec4769223058f3727de3d24db4cf38588b8a4
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-13 13:37:22 -0800

    map through data to create array of posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,17 +7,19 @@ import axios from "axios";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts/10").then((response) => console.log(response.data));
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      this.setState({ posts: response.data });
+    });
   }
   render() {
+    const posts = this.state.posts.map((post) => <Post title={post.title} key={post.id} />);
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
