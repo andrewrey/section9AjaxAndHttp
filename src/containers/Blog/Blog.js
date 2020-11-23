da71a4ae5dfb96a9e1e95912413e3dcc98354a0b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-22 17:00:31 -0800

    added react-router-dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 // import axios from "axios";
 import axios from "../../axios";
 import "./Blog.css";
@@ -56,12 +53,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
