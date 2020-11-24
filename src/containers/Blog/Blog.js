356b2df7b61dcf201cf8d79f43497ff33f721f82
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-23 16:24:33 -0800

    converted a tags to link tags for react

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 // import axios from "axios";
 import axios from "../../axios";
 import "./Blog.css";
@@ -14,10 +14,18 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
