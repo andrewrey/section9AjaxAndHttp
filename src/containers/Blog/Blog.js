c67b4fc318686145469d99c27a857cd6a2cc3ef9
Andrew Reynolds <andrew_reynolds@mac.com>
2020-11-29 22:18:01 -0800

    holder

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -30,6 +30,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
+        <p>Holder</p>
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" exact component={NewPost} />
       </div>
